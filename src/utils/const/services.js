30c1ce3515e3133264fc4d39a0978331738779cd
BalaguerDev <balaguer.dev@gmail.com>
2024-11-22 19:33:18 +0100

    feat(HomePage): :sparkles: hero and service card
    hero and service card, TODO fix img services

---
+++ src/utils/const/services.js
@@ -0,0 +1,25 @@
+export const services = [
+    {
+      title: "Maquillaje y Peinados de Novia",
+      description:
+        "Luce radiante en tu gran día con nuestro servicio de maquillaje y peinado personalizado. Expertas en novias, te asesoramos para encontrar el look perfecto que resalte tu belleza natural.",
+      imageSrc: "/maquillaje-natural-para-novia-en-barcelona.png",
+      link: "/novias",
+    },
+    {
+      title: "Cursos Profesionales y Automaquillaje",
+      description:
+        "Aprende las técnicas más avanzadas de maquillaje y peinado con nuestros cursos profesionales. Ofrecemos clases para todos los niveles.",
+      imageSrc: "/clases-de-automaquillaje-barcelona.jpg",
+      link: "/cursos-automaquillaje-autopeinados",
+    },
+    {
+      title: "Asesoría de Imagen",
+      description:
+        "Descubre tu estilo personal y aprende a potenciar tu imagen con nuestra asesoría de imagen integral.",
+      imageSrc: "/maquillaje-natural-para-novia-en-barcelona.png",
+      link: "/asesoria-imagen",
+    },
+  ];
+// TODO modificar imagenes y coinvertirlas a webp
+  
---
+++ src/utils/const/site-info.js
@@ -0,0 +1,4 @@
+export const siteInfo = {
+    title: 'Maquillaje y Peinados para Novias en Barcelona con Silvia Mendoza',
+    description: '¿Buscas una maquilladora para tu boda en Barcelona? Silvia Mendoza Makeup ofrece servicios de maquillaje y peinado para novias e invitadas. Además se imparten cursos de automaquillaje y recogidos tanto para tu dia a dia, como para un evento social.'
+}
